57ebc3a7fceb2024be6cceb5736167eaf7496852
Adedayo Adedapo <daniel@deewanstudios.com>
2018-12-03 12:33:16 +0000

    Built the enquiry form section of the product enquiry page

---
+++ application/views/templates/layouts/product-enquiry-details.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * This is the product enquiry file. This file deals with with all implementations of member variable and methods required for product enquiry by the user to be sent to the owner of the business.
+ * PHP version PHP 7.2.1.
+ *
+ * @category Website
+ * @package  WheeliesBikes_Product_Enquiry_File
+ * @author   Adedayo Adedapo <ade.adedapo9@gmail.com>
+ * @license  DeewanstudiosLTD deewanstudios.com
+ * @link     http://url.com
+ */
